5d79d833fcb417874aae6c86d86333397dfe2552
Kasmi Thapa <kasmithapa00@gmail.com>
2022-09-08 18:55:33 +0545

    loop

---
+++ loop.py
@@ -0,0 +1,12 @@
+#even_num_list=[2,4,6,8,10]
+
+#range (s,s,s) --> start, stop, step
+for num in range(0,10+1,1):
+     if num %2 ==0:
+        print(f"the value of num is :{num}")
+
+        
+days_list =['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
+
+for day in days_list:
+        print(f"The single day is:{day}")
